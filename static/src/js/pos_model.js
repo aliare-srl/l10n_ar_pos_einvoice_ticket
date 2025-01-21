78dfed62f1434fec314b3809ab16b4812715ffa2
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-01-21 16:46:29 -0300

    Updated module to allow refunds to the same client

--- static/src/js/pos_model.js
+++ static/src/js/pos_model.js
@@ -2,6 +2,8 @@
 import { Order } from "@point_of_sale/app/store/models";
 import { PosStore } from "@point_of_sale/app/store/pos_store";
 import { patch } from "@web/core/utils/patch";
+import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
+
 patch(PosStore.prototype, {
     async _flush_orders(orders, options = {}) {
         var self = this;
@@ -82,10 +84,12 @@ patch(PosStore.prototype, {
 patch(Order.prototype, {
     setup() {
         super.setup(...arguments);
-        var default_customer = this.pos.config.default_partner_id;
-        var default_customer_by_id = this.pos.db.get_partner_by_id(default_customer[0]);
-        if(default_customer_by_id){
-            this.set_partner(default_customer_by_id);
+        if (!this.get_partner()) {
+            var default_customer = this.pos.config.default_partner_id;
+            var default_customer_by_id = this.pos.db.get_partner_by_id(default_customer[0]);
+            if (default_customer_by_id) {
+                this.set_partner(default_customer_by_id);
+            }
         }
         var default_to_invoice = this.pos.config.pos_auto_invoice;
         this.set_to_invoice(default_to_invoice);        
@@ -129,4 +133,126 @@ patch(Order.prototype, {
         }
         return result;
     },
+});
+
+patch(TicketScreen.prototype, {
+    async onDoRefund() {
+        const order = this.getSelectedOrder();
+
+        if (order && this._doesOrderHaveSoleItem(order)) {
+            if (!this._prepareAutoRefundOnOrder(order)) {
+                // Don't proceed on refund if preparation returned false.
+                return;
+            }
+        }
+
+        if (!order) {
+            this._state.ui.highlightHeaderNote = !this._state.ui.highlightHeaderNote;
+            return;
+        }
+
+        const partner = order.get_partner();
+        if (!partner) {
+            console.log("No se encontró un cliente asociado, utilizando Consumidor Final");
+            // Asignar Consumidor Final si no hay cliente
+        }
+        const allToRefundDetails = this._getRefundableDetails(partner);
+        if (allToRefundDetails.length == 0) {
+            this._state.ui.highlightHeaderNote = !this._state.ui.highlightHeaderNote;
+            return;
+        }
+
+        const invoicedOrderIds = new Set(
+            allToRefundDetails
+                .filter(
+                    (detail) =>
+                        this._state.syncedOrders.cache[detail.orderline.orderBackendId]?.state ===
+                        "invoiced"
+                )
+                .map((detail) => detail.orderline.orderBackendId)
+        );
+
+        if (invoicedOrderIds.size > 1) {
+            this.popup.add(ErrorPopup, {
+                title: _t("Multiple Invoiced Orders Selected"),
+                body: _t(
+                    "You have selected orderlines from multiple invoiced orders. To proceed refund, please select orderlines from the same invoiced order."
+                ),
+            });
+            return;
+        }
+
+        // The order that will contain the refund orderlines.
+        // Use the destinationOrder from props if the order to refund has the same
+        // partner as the destinationOrder.
+        const destinationOrder =
+            this.props.destinationOrder &&
+            partner === this.props.destinationOrder.get_partner() &&
+            !this.pos.doNotAllowRefundAndSales()
+                ? this.props.destinationOrder
+                : this._getEmptyOrder(partner);
+
+        // Add orderline for each toRefundDetail to the destinationOrder.
+        const originalToDestinationLineMap = new Map();
+
+        // First pass: add all products to the destination order
+        for (const refundDetail of allToRefundDetails) {
+            const product = this.pos.db.get_product_by_id(refundDetail.orderline.productId);
+            const options = this._prepareRefundOrderlineOptions(refundDetail);
+            const newOrderline = await destinationOrder.add_product(product, options);
+            originalToDestinationLineMap.set(refundDetail.orderline.id, newOrderline);
+            refundDetail.destinationOrderUid = destinationOrder.uid;
+        }
+        // Second pass: update combo relationships in the destination order
+        for (const refundDetail of allToRefundDetails) {
+            const originalOrderline = refundDetail.orderline;
+            const destinationOrderline = originalToDestinationLineMap.get(originalOrderline.id);
+            if (originalOrderline.comboParent) {
+                const comboParentLine = originalToDestinationLineMap.get(
+                    originalOrderline.comboParent.id
+                );
+                if (comboParentLine) {
+                    destinationOrderline.comboParent = comboParentLine;
+                }
+            }
+            if (originalOrderline.comboLines && originalOrderline.comboLines.length > 0) {
+                destinationOrderline.comboLines = originalOrderline.comboLines.map((comboLine) => {
+                    return originalToDestinationLineMap.get(comboLine.id);
+                });
+            }
+        }
+        
+        //Add a check too see if the fiscal position exist in the pos
+        if (order.fiscal_position_not_found) {
+            this.showPopup("ErrorPopup", {
+                title: _t("Fiscal Position not found"),
+                body: _t(
+                    "The fiscal position used in the original order is not loaded. Make sure it is loaded by adding it in the pos configuration."
+                ),
+            });
+            return;
+        }
+        destinationOrder.fiscal_position = order.fiscal_position;
+        // Set the partner to the destinationOrder.
+        //this.setPartnerToRefundOrder(partner, destinationOrder);
+        if (partner) {
+            //console.log(partner, destinationOrder)
+            destinationOrder.set_partner(partner);  // Forzar la asignación del cliente original
+        } else {
+            //console.log("No se encontró un cliente asociado, utilizando Consumidor Final");
+            const default_customer = this.pos.config.default_partner_id;
+            const default_customer_by_id = this.pos.db.get_partner_by_id(default_customer[0]);
+            if (default_customer_by_id) {
+                destinationOrder.set_partner(default_customer_by_id);
+            }
+        }
+        //console.log("Destination order:",destinationOrder)
+
+        if (this.pos.get_order().cid !== destinationOrder.cid) {
+            this.pos.set_order(destinationOrder);
+        }
+        await this.addAdditionalRefundInfo(order, destinationOrder);
+
+        this.closeTicketScreen();
+    }
 });

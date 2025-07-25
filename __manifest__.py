1a89ea8fd0e11ad554716544fdafc1ba359ea0e0
Aliaresrl <143626551+Aliaresrl@users.noreply.github.com>
2025-07-25 14:58:08 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -14,12 +14,12 @@
         'views/res_config_settings_views.xml',
     ],
     'assets': {
-        "point_of_sale._assets_pos": [
-            "/l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js",
-            "/l10n_ar_pos_einvoice_ticket/static/src/js/PaymentScreen.js",
-            "/l10n_ar_pos_einvoice_ticket/static/src/css/pos_receipts.css",
-            "/l10n_ar_pos_einvoice_ticket/static/src/xml/pos_ticket.xml",
-        ],
-    },
+    "point_of_sale.assets": [
+        "/l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js",
+        "/l10n_ar_pos_einvoice_ticket/static/src/js/PaymentScreen.js",
+        "/l10n_ar_pos_einvoice_ticket/static/src/css/pos_receipts.css",
+        "/l10n_ar_pos_einvoice_ticket/static/src/xml/pos_ticket.xml",
+    ],
+   },
     'installable': True,
 }

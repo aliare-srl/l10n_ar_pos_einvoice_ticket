78055a81b47a140241ffb971295bc03d49747103
Aliaresrl <143626551+Aliaresrl@users.noreply.github.com>
2025-07-28 11:42:48 -0300

    Update PaymentScreen.js

--- static/src/js/PaymentScreen.js
+++ static/src/js/PaymentScreen.js
@@ -1,20 +1,23 @@
 /** @odoo-module */
+
 import { PaymentScreen } from "@point_of_sale/app/screens/payment_screen/payment_screen";
 import { patch } from "@web/core/utils/patch";
-patch(PaymentScreen.prototype, {
-	setup() {
+
+patch(PaymentScreen, {
+    setup() {
         super.setup(...arguments);
     },
-	shouldDownloadInvoice() {
+    shouldDownloadInvoice() {
         if (this.pos.config.pos_stop_invoice_print) {
-        	return false;
+            return false;
         } else {
-            return super.shouldDownloadInvoice(); 
+            return super.shouldDownloadInvoice();
         }
-    },	
+    },
     async afterOrderValidation(suggestToSync = true) {
         // timeout para carga de datos fiscales
         await new Promise(resolve => setTimeout(resolve, 1000));
         return super.afterOrderValidation(...arguments);
     },
 });
+

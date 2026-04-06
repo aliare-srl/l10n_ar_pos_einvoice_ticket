4ab24ded5e42ca23653ddd0dfd8386dcbab0af3e
ASR <68700448+rolandoaliare@users.noreply.github.com>
2026-04-06 15:50:53 -0300

    l10n_ar_pos_einvoice_ticket - FIX: Corrige XPaths y nombres de campos AFIP en template

--- __manifest__.py
+++ __manifest__.py
@@ -14,13 +14,8 @@
         'views/res_config_settings_views.xml',
     ],
     'assets': {
-        "point_of_sale.assets": [
-            "/l10n_ar_pos_einvoice_ticket/static/src/js/pos_model.js",
-            "/l10n_ar_pos_einvoice_ticket/static/src/js/PaymentScreen.js",
-            "/l10n_ar_pos_einvoice_ticket/static/src/css/pos_receipts.css",
-        ],
-        "web.assets_qweb": [
-            "/l10n_ar_pos_einvoice_ticket/static/src/xml/pos_ticket.xml",
+        "point_of_sale._assets_pos": [
+            "l10n_ar_pos_einvoice_ticket/static/src/**/*",
         ],
     },
     'installable': True,

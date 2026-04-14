3a49b4098ad44c28c849189ed9ae5b1e422ce330
ASR <68700448+rolandoaliare@users.noreply.github.com>
2026-04-14 12:14:12 -0300

    l10n_ar_pos_einvoice_ticket - FIX: Cargar campos l10n_latam del partner en sesion POS

--- models/pos_session.py
+++ models/pos_session.py
@@ -9,7 +9,14 @@ class PosSession(models.Model):
             "l10n_ar_afip_responsibility_type_id",
             "l10n_ar_gross_income_number",
             "l10n_ar_afip_start_date",
-            "street",
             "parent_id",
+        ]
+        return res
+
+    def _loader_params_res_partner(self):
+        res = super()._loader_params_res_partner()
+        res["search_params"]["fields"] += [
+            "l10n_latam_identification_type_id",
+            "l10n_ar_afip_responsibility_type_id",
         ]
         return res
